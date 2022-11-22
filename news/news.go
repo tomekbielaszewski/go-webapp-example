6e8546072e478bdc23ff78b35b588abe35295238
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2022-11-22 18:00:12 +0100

    getting "everything" from news api

--- news/news.go
+++ news/news.go
@@ -1,11 +1,36 @@
 package news
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
 
 type Client struct {
 	httpClient *http.Client
 	key        string
 	pageSize   int8
+}
+
+type Results struct {
+	Status       string `json:"status"`
+	TotalResults int    `json:"totalResults"`
+	Articles     []struct {
+		Source struct {
+			ID   interface{} `json:"id"`
+			Name string      `json:"name"`
+		} `json:"source"`
+		Author      string    `json:"author"`
+		Title       string    `json:"title"`
+		Description string    `json:"description"`
+		URL         string    `json:"url"`
+		URLToImage  string    `json:"urlToImage"`
+		PublishedAt time.Time `json:"publishedAt"`
+		Content     string    `json:"content"`
+	} `json:"articles"`
 }
 
 func DefaultNewsClient(apiKey string) *Client {
@@ -18,4 +43,27 @@ func NewNewsClient(apiKey string, pageSize int8, httpClient *http.Client) *Clien
 		key:        apiKey,
 		pageSize:   pageSize,
 	}
+}
+
+func (c *Client) FetchEverything(query, page string) (*Results, error) {
+	encodedQuery := url.QueryEscape(query)
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", encodedQuery, c.pageSize, page, c.key)
+	resp, err := c.httpClient.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	res := &Results{}
+	return res, json.Unmarshal(body, res)
 }
