80587b005ca6e2102efde9e8404217efe044ac07
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2022-11-22 06:13:07 +0100

    loading server port from env file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-webapp-example
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
 	"os"
 )
@@ -11,12 +12,37 @@ func indexHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte(resString))
 }
 
+const PortVar = "PORT"
+const DefaultPort = "8080"
+
 func main() {
+	err := LoadEnv()
+	if err != nil {
+		fmt.Printf("Cannot load .env file and cannot set default PORT. %s\n", err)
+	}
+	port := GetPort()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":"+port, mux)
 	if err != nil {
 		fmt.Println(fmt.Errorf("problem when starting server: %s", err))
 		os.Exit(-1)
 	}
+}
+
+func GetPort() string {
+	port := os.Getenv(PortVar)
+	if port == "" {
+		port = DefaultPort
+	}
+	return port
+}
+
+func LoadEnv() error {
+	err := godotenv.Load()
+	if err != nil {
+		err = os.Setenv(PortVar, DefaultPort)
+	}
+	return err
 }
